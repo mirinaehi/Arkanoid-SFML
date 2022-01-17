b0df49cddad594773acfcbf3ff9ed6e0666f088e
labyw <garook@e-mirim.hs.kr>
2022-01-17 11:44:34 +0900

    이미지 화면에 띄우기

--- main.cpp
+++ main.cpp
@@ -13,6 +13,35 @@ void main(void)
 	t3.loadFromFile("images/ball.png");
 	t4.loadFromFile("images/paddle.png");
 
+	Sprite sBackground(t2), sBall(t3), sPaddle(t4);
+	sPaddle.setPosition(300, 400);
+	sBall.setPosition(300, 300);
+
+	Sprite block[1000];
+	int n = 0;
+	for(int i=1; i<=10; i++)
+		for(int j=0; j<=10; j++) {
+			block[n].setTexture(t1);
+			block[n].setPosition(i * 43, j * 20);
+			n++;
+		}
+
+	while(app.isOpen()) {
+		Event e;
+		while (app.pollEvent(e)) {
+			if (e.type == Event::Closed)
+				app.close();
+		}
+		app.clear();
+		app.draw(sBackground);
+		app.draw(sBall);
+		app.draw(sPaddle);
+
+		for (int i = 0; i < n; i++)
+			app.draw(block[i]);
+
+		app.display();
+	}
 }
 
 //int main()
